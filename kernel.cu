43ef12516c5fcf5f452d31311a40d87371fd9e78
Hemendra Ghosn <hrh10@mail.aub.edu>
2024-02-23 14:39:32 +0200

    coarsening changes

--- kernel.cu
+++ kernel.cu
@@ -42,24 +42,25 @@ void histogram_gpu_private(unsigned char* image_d, unsigned int* bins_d, unsigne
 
 __global__ void histogram_private_coarse_kernel(unsigned char* image, unsigned int* bins, unsigned int width, unsigned int height) {
 
-    __shared__ unsigned int private_bins[256]; 
-	unsigned int coarseningFactor = 64;
+    __shared__ unsigned int private_bins[256]; // Private histogram bins for each block
 
-
-    for (int i = 0; i < 256; ++i) {
-        private_bins[i] = 0;
+    // Initialize private histogram bins to zero
+    for (int j = 0; j < 256; j++) {
+        private_bins[j] = 0;
     }
 
-    __syncthreads();
+    __syncthreads(); // Ensure all threads have initialized private histogram bins
 
-    unsigned int tid = (blockIdx.x * blockDim.x * coarseningFactor) + threadIdx.x;
+    unsigned int tid = (blockIdx.x * blockDim.x * 64) + threadIdx.x;
     while (tid < width * height) {
         unsigned char b = image[tid];
-        atomicAdd(&private_bins[b], 1);
-        tid += blockDim.x;
+        atomicAdd(&private_bins[b], 1); // Update private histogram bins atomically
+        tid += blockDim.x; // Increment tid by total number of threads
     }
 
-    __syncthreads();
+    __syncthreads(); // Ensure all threads have finished updating private histogram bins
+
+    // Commit non-zero bin counts to the global copy of the histogram in parallel
     if (threadIdx.x < 256) {
         atomicAdd(&bins[threadIdx.x], private_bins[threadIdx.x]);
     }
